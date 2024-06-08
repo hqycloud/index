23a4e396606cc85a0ac9f81c289b00c765d55b48
hqycloud <huangqiyuyyds@gmail.com>
2024-06-08 16:05:21 +0800

    modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 function show_runtime() {
-    window.setTimeout("show_runtime()", 1000);
-    X = new Date("6/2/2024 14:00:00");
-    Y = new Date();
-    T = (Y.getTime() - X.getTime());
-    M = 24 * 60 * 60 * 1000;
-    a = T / M;
-    A = Math.floor(a);
-    b = (a - A) * 24;
-    B = Math.floor(b);
-    c = (b - B) * 60;
-    C = Math.floor((b - B) * 60);
-    D = Math.floor((c - C) * 60);
-    runtime_span.innerHTML = "网站在各种灾难中运行了: " + A + "天" + B + "小时" + C + "分" + D + "秒"
+      const X = new Date("6/2/2024 14:00:00");
+      const Y = new Date();
+      const T = (Y.getTime() - X.getTime());
+      const M = 24 * 60 * 60 * 1000;
+      let a = T / M;
+      const A = Math.floor(a);
+      let b = (a - A) * 24;
+      const B = Math.floor(b);
+      let c = (b - B) * 60;
+      const C = Math.floor((b - B) * 60);
+      const D = Math.floor((c - C) * 60);
+
+      const runtimeSpan = document.getElementById("runtime_span");
+      runtimeSpan.innerHTML = "网站在各种灾难中运行了: " + A + "天" + B + "小时" + C + "分" + D + "秒";
 }
-show_runtime();
+window.setInterval(show_runtime, 1000); // 每秒更新一次
